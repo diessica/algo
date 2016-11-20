339fb66d5385212ddc42fb7a5917dbb988e4f443
Diéssica Gurskas <diessicode@gmail.com>
2016-11-20 12:37:52 -0200

    Add repeat

---
+++ repeat.js
@@ -0,0 +1,19 @@
+/*
+ * Repeat the given string n times.
+ * Return an empty string if n is not a positive number.
+ */
+
+function repeat(str, n) {
+  let result = ''
+
+  if (n < 0) return ''
+  if (n === 1) return str
+
+  while (result.length < str.length * n) {
+    result += str
+  }
+
+  return result
+}
+
+repeat('abc', 3) // => abcabcabc
