42259c4c98fbdc469d137747197b9516b13ca217
Diéssica Gurskas <diessicode@gmail.com>
2016-11-26 18:38:30 -0200

    Refactor diffArray

--- diff-array.js
+++ diff-array.js
@@ -1,8 +1,8 @@
 /*
- * Rreturn the symmetric difference of the two arrays.
+ * Return the symmetric difference of the two arrays.
  */
 
-function diffArray(arr, arr2) {
+function diffArray (arr, arr2) {
   return arr
     .filter(el => !arr2.includes(el))
     .concat(arr2.filter(el => !arr.includes(el)))
