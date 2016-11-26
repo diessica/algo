b5cdde78542fe34fe036c61aa77dc7e8185d3c19
Diéssica Gurskas <diessicode@gmail.com>
2016-11-26 10:23:06 -0200

    Add chunkArray

---
+++ chunk-array.js
@@ -0,0 +1,18 @@
+/*
+ * Write a function that splits an array (first argument) into groups
+ * the length of size (second argument) and returns them as a two-dimensional array.
+ */
+
+function chunkArray (arr, size) {
+  let result = []
+  let pos = 0
+  let total = arr.length
+
+  while (pos < total) {
+    result.push(arr.slice(pos, pos += size))
+  }
+
+  return result
+}
+
+chunkArray(["a", "b", "c", "d", "e", "f"], 2) // => [["a", "b"], ["c", "d"]]
