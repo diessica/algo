d37b7a80fd617966f2cb72b14b7e6ce444117b7b
Diéssica Gurskas <diessicode@gmail.com>
2016-11-26 11:33:30 -0200

    Refactor mutation to hasLetters

---
+++ has-letters.js
@@ -0,0 +1,15 @@
+/*
+ * Return true if the string in the first element of the array contains all
+ * of the letters of the string in the second element of the array.
+ */
+
+function hasLetters ([str, letters]) {
+  return letters
+    .toLowerCase()
+    .split('')
+    .every((letter) => str.toLowerCase().indexOf(letter) !== -1)
+}
+
+hasLetters(["hello", "neo"]) // => false
+hasLetters(["Mary", "Army"]) // => true
+hasLetters(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]) // => true
